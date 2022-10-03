960017775be16a9265467d1e4c7d0145c2ad4be7
João Pedro Alves <jotalmeida007@hotmail.com>
2022-10-03 16:45:20 -0300

    feat: :art: add supplier proto

--- lib/functions/pb/seller_center/orders.pb.ex
+++ lib/functions/pb/seller_center/orders.pb.ex
@@ -2,42 +2,43 @@ defmodule OrdersGRPC.ListOrdersRequest do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :aggregate_suppliers, 1,
+  field(:aggregate_suppliers, 1,
     repeated: true,
     type: OrdersGRPC.AggregateSuppliers,
     json_name: "aggregateSuppliers"
+  )
 
-  field :status, 2, type: :string
+  field(:status, 2, type: :string)
 end
 
 defmodule OrdersGRPC.AggregateSuppliers do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :suppliers, 1, repeated: true, type: :string
-  field :stale_days, 2, type: :int32, json_name: "staleDays"
+  field(:suppliers, 1, repeated: true, type: :string)
+  field(:stale_days, 2, type: :int32, json_name: "staleDays")
 end
 
 defmodule OrdersGRPC.ListOrdersResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :orders, 1, repeated: true, type: OrdersGRPC.Order
+  field(:orders, 1, repeated: true, type: OrdersGRPC.Order)
 end
 
 defmodule OrdersGRPC.Order do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :code, 1, type: :string
-  field :supplier_id, 2, type: :int32, json_name: "supplierId"
+  field(:code, 1, type: :string)
+  field(:supplier_id, 2, type: :int32, json_name: "supplierId")
 end
 
 defmodule OrdersGRPC.ListStaleOrders.Service do
   @moduledoc false
   use GRPC.Service, name: "OrdersGRPC.ListStaleOrders", protoc_gen_elixir_version: "0.11.0"
 
-  rpc :execute, OrdersGRPC.ListOrdersRequest, OrdersGRPC.ListOrdersResponse
+  rpc(:execute, OrdersGRPC.ListOrdersRequest, OrdersGRPC.ListOrdersResponse)
 end
 
 defmodule OrdersGRPC.ListStaleOrders.Stub do
---
+++ lib/functions/pb/suppliers.pb.ex
@@ -0,0 +1,41 @@
+defmodule SuppliersGRPC.ListSupplierRequest do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field(:id, 1, type: :int32)
+end
+
+defmodule SuppliersGRPC.ListSupplierResponse do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field(:supplier, 1, type: SuppliersGRPC.Supplier)
+end
+
+defmodule SuppliersGRPC.Supplier do
+  @moduledoc false
+  use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
+
+  field(:name, 1, type: :string)
+  field(:contact_name, 2, type: :string, json_name: "contactName")
+  field(:active, 3, type: :bool)
+  field(:trading_fee, 4, type: :int32, json_name: "tradingFee")
+  field(:cnpj, 5, type: :string)
+  field(:email, 6, type: :string)
+  field(:company_name, 7, type: :string, json_name: "companyName")
+  field(:nf_stale_days, 8, type: :int32, json_name: "nfStaleDays")
+  field(:confirmation_stale_days, 9, type: :int32, json_name: "confirmationStaleDays")
+  field(:id, 10, type: :int32)
+end
+
+defmodule SuppliersGRPC.ListSupplier.Service do
+  @moduledoc false
+  use GRPC.Service, name: "SuppliersGRPC.ListSupplier", protoc_gen_elixir_version: "0.11.0"
+
+  rpc(:execute, SuppliersGRPC.ListSupplierRequest, SuppliersGRPC.ListSupplierResponse)
+end
+
+defmodule SuppliersGRPC.ListSupplier.Stub do
+  @moduledoc false
+  use GRPC.Stub, service: SuppliersGRPC.ListSupplier.Service
+end
