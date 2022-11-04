c5b72bce9bfb07589790ee53c480eb2c7d615909
RoxoFoxo <gab.apfprado@gmail.com>
2022-11-04 16:23:42 -0300

    chore: :fire: remove supplier_id and add supplier_name and supplier_email to summary response

--- lib/functions/pb/seller_center/list_order_status_summaries.pb.ex
+++ lib/functions/pb/seller_center/list_order_status_summaries.pb.ex
@@ -7,17 +7,18 @@ defmodule OrdersGRPC.ListSummariesResponse do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:summaries, 1, repeated: true, type: OrdersGRPC.Summary)
+  field :summaries, 1, repeated: true, type: OrdersGRPC.Summary
 end
 
 defmodule OrdersGRPC.Summary do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field(:supplier_id, 1, type: :int32, json_name: "supplierId")
-  field(:amount_created, 2, type: :int32, json_name: "amountCreated")
-  field(:amount_confirmed, 3, type: :int32, json_name: "amountConfirmed")
-  field(:amount_paid, 4, type: :int32, json_name: "amountPaid")
+  field :supplier_name, 1, type: :string, json_name: "supplierName"
+  field :supplier_email, 2, type: :string, json_name: "supplierEmail"
+  field :amount_created, 3, type: :int32, json_name: "amountCreated"
+  field :amount_confirmed, 4, type: :int32, json_name: "amountConfirmed"
+  field :amount_paid, 5, type: :int32, json_name: "amountPaid"
 end
 
 defmodule OrdersGRPC.ListOrderStatusSummaries.Service do
@@ -26,7 +27,7 @@ defmodule OrdersGRPC.ListOrderStatusSummaries.Service do
     name: "OrdersGRPC.ListOrderStatusSummaries",
     protoc_gen_elixir_version: "0.11.0"
 
-  rpc(:execute, OrdersGRPC.ListSummariesRequest, OrdersGRPC.ListSummariesResponse)
+  rpc :execute, OrdersGRPC.ListSummariesRequest, Grpc.Orders.ListSummariesResponse
 end
 
 defmodule OrdersGRPC.ListOrderStatusSummaries.Stub do
