3d6fd8058403cfefb1d87e77df2c8bee133cafc7
RoxoFoxo <gab.apfprado@gmail.com>
2022-11-08 11:14:19 -0300

    feat: :pencil2: remove supplier from supplier_attrs fields

--- lib/functions/pb/seller_center/supplier_attrs.pb.ex
+++ lib/functions/pb/seller_center/supplier_attrs.pb.ex
@@ -2,7 +2,7 @@ defmodule OrdersGRPC.SupplierAttrs do
   @moduledoc false
   use Protobuf, protoc_gen_elixir_version: "0.11.0", syntax: :proto3
 
-  field :supplier_id, 1, type: :int32, json_name: "supplierId"
-  field :supplier_name, 2, type: :string, json_name: "supplierName"
-  field :supplier_email, 3, type: :string, json_name: "supplierEmail"
+  field :id, 1, type: :int32
+  field :name, 2, type: :string
+  field :email, 3, type: :string
 end
